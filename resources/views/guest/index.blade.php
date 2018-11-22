2345c55d9baadde520fbb712cfde7dad557ef10a
wahyuadepratama <1511521024@student.unand.ac.id>
2018-11-22 22:13:16 +0700

    revisi v2.6 ~ repair footer

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -110,6 +110,76 @@
 </div>
 
 <!-- START FOOTER AREA -->
+<footer id="footer" class="footer-area footer-2">
+    <div class="footer-top footer-top-2 text-center ptb-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ul class="footer-menu-2">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/portofolio">Portofolio</a></li>
+                        <li><a href="/theme">Design Theme</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom footer-bottom-2 text-center gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4" style="margin-top: 1%; margin-bottom: 1%">
+                    <div class="copyright-text copyright-text-2">
+                        <p>&copy; <a href="https://wapydesign.com" target="_blank">WapyDesign</a> 2018. All Rights Reserved.</p>
+                    </div>
+                </div>
+                <div class="col-md-4" style="margin-top: 2%; margin-bottom: 2%">
+                    <ul class="footer-social footer-social-3">
+                        <li>
+                            <a class="google-plus" href="/contact" title="Email"><i class="zmdi zmdi-email"></i></a>
+                        </li>
+                        <li>
+                            <a class="google-plus" href="/contact" title="Google"><i class="zmdi zmdi-google-plus"></i></a>
+                        </li>
+                        <li>
+                            <a class="google-plus" href="/contact" title="Instagram"><i class="zmdi zmdi-instagram"></i></a>
+                        </li>
+                        <li>
+                          <a class="google-plus" href="/contact" title="Whatsapp"><i class="zmdi zmdi-whatsapp"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <style media="screen">
+                  .grid-bank{
+                    display:grid;
+                    grid-template-columns: 1fr 1fr 1fr;
+                    padding-top: 10px;
+                    grid-template-rows: auto;
+                    grid-template-areas:
+                    "bri bni mandiri"
+                  }
+                  .bri{grid-area: bri; }
+                  .bni{grid-area: bni}
+                  .mandiri{grid-area:mandiri}
+                </style>
+                <div class="col-md-4 col-sm-3" style="margin-top: 1%; margin-bottom: 1%">
+                    <div class="grid-bank">
+                    <div class="bni">
+                      <a href="#"><img src="{{URL::asset('guest/images/icons/bni.png')}}" width="80%" alt=""></a>
+                    </div>
+                    <div class="bri">
+                      <a href="#"><img src="{{URL::asset('guest/images/icons/bri.png')}}"  width="80%" alt=""></a>
+                    </div>
+                    <div class="mandiri">
+                      <a href="#"><img src="{{URL::asset('guest/images/icons/mandiri.png')}}" width="80%" alt=""></a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 
 
 @endsection
