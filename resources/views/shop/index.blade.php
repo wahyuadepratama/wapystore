bd3ebd4a1eb2adde6b17f6eaa6dbac2d745c594d
wahyuadepratama <1511521024@student.unand.ac.id>
2018-12-14 14:33:23 +0700

    revisi v3.1.4

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -40,6 +40,7 @@
                     <option value="Java Seven">Java Seven</option>
                     <option value="Inflico">Inficlo</option>
                     <option value="Blackelly">Blackelly</option>
+                    <option value="Cuci Gudang">Cuci Gudang</option>
                   </select>
                   <button style="margin-top:1%;margin-bottom:5%" type="submit" class="btn-sm btn-warning form-control"><b>Filter Brand</b></a>
                 </div>
